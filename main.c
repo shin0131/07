e2de24f46b5649342671fc621a7cabe34569007e
shin0131 <shinyoung@sookmyung.ac.kr>
2022-10-18 16:40:29 +0900

    03

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-	for(i = 0; i < 5; i++)
-	{
-		int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
-	return 0;
+	int i = 10;
+	printf("함수 호출전 i = %d\n", i);
+	inc(i);
+	printf("함수 호출후 i = %d\n", i);
+	return 0; 
+}
+
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
